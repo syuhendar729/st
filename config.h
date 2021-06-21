91324d4b9731a05dea471a7d733453d75653aa51
Syuhada Rantisi <suadarantisi@gmail.com>
2021-06-21 22:55:09 +0700

    Solved membuat st bisa powerline nya. Install font Hack dan tambahkan font2 nya

--- config.h
+++ config.h
@@ -8,8 +8,8 @@
 static char *font = "Liberation Mono:pixelsize=16:antialias=true:autohint=true";
 /* Spare fonts */
 static char *font2[] = {
-	"Inconsolata for Powerline:pixelsize=12:antialias=true:autohint=true",
-	"Hack Nerd Font Mono:pixelsize=11:antialias=true:autohint=true",
+	"Inconsolata:pixelsize=12:antialias=true:autohint=true",
+	"Hack:pixelsize=16:antialias=true:autohint=true",
 };
 
 
